2b5769c0488b079f4a466af77001d28d865f5418
yolominwoo <minwoo9594@gmail.com>
2023-05-28 22:15:19 +0900

    short char1 code

--- db/Java_Web/server.js
+++ db/Java_Web/server.js
@@ -1,24 +1,22 @@
 const { Console } = require('console');
 const express = require('express');
 const multer = require('multer');
-const app = express();  //; 없어도됨
-//const myDropzone = require("script");
+const app = express();
 app.use(express.static(__dirname));
-//const appModule = require('./app');
-// app.use('/app.js', express.static(__dirname + '/app.js'));
+app.use('/app.js', express.static(__dirname + '/app.js'));
 const {exec} = require('child_process');
 
 
 
 
-const path = require('path'); //없어도 실행됨(책에서 있음)
+const path = require('path');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./"); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+      cb(null, "./");
     },
     filename: function (req, file, cb) {
-      cb(null, 'inputFile.txt') // cb 콜백함수를 통해 전송된 파일 이름 설정
+      cb(null, 'inputFile.txt') 
       console.log(file.originalname)
     }
 })
@@ -67,7 +65,7 @@ app.get('/first',async function(req,res){
                 res.sendFile(__dirname + '/chart1.html');
             }
             console.log(`stdout: ${stdout}`);
-            // console.error(`stderr: ${stderr}`);
+         
           });
 
         
@@ -84,9 +82,6 @@ app.get('/first',async function(req,res){
     });
 });
 
-// app.get('/app.js', function (req, res) {
-//     res.sendFile(path.join(__dirname, '/app.js'));
-// });
 
 
 app.get('/', function (req, res) {
@@ -100,26 +95,21 @@ app.get('/chart', function (req, res) {
 });
 
 
-// app.post('/second', upload.single('profile', maxFileCount), function(req, res){
-//     console.log(req.file);
-//     res.redirect('/'); // 파일 업로드 완료 후 페이지 이동
-// });
 
-const maxFileCount = 1; // 최대 업로드 가능한 파일 수
+const maxFileCount = 1; 
 
 app.post('/', upload.array('profile', maxFileCount), function(req, res){
-    // 파일 업로드 수가 최대 허용 수를 초과한 경우
+   
     if (req.files && req.files.length > maxFileCount) {
-        // 업로드된 파일 제거
+       
         for (let i = maxFileCount; i < req.files.length; i++) {
             const filePath = req.files[i].path;
-            fs.unlinkSync(filePath); // 파일 삭제
+            fs.unlinkSync(filePath); 
         }
         res.status(400).send(`Only ${maxFileCount} file(s) can be uploaded.`);
         return;
     }
-    
-    // 파일 저장 성공 시
+   
     if (req.files && req.files.length > 0) {
         res.send('Uploaded! : ' + req.files[0].originalname);
         console.log(req.files[0]);
@@ -127,16 +117,9 @@ app.post('/', upload.array('profile', maxFileCount), function(req, res){
         var complete = "업로드 완료";
         console.log(complete);
         
-        // // 드롭존 초기화
-        // myDropzone.removeAllFiles();
-        // myDropzone.enable(); // 드롭존 활성화
-        
-        // // 한 개의 파일만 선택 가능한 안내문 출력
-        // var message = document.createElement('p');
-        // message.innerText = 'You can only select one file.';
-        // document.querySelector('.dropzone').appendChild(message);
+    
     }
-    // 파일 저장 실패 시
+   
     else {
         res.status(400).send('File upload failed.');
     }
